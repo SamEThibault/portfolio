1e4134dc8f4a186c98b7d730f57de3ae1d250497
Samuel Emard-Thibault <sam5thibault@gmail.com>
2025-04-17 15:54:41 -0400

    fix perm error

--- shell_scripts/redeploy-site.sh
+++ shell_scripts/redeploy-site.sh
@@ -9,8 +9,8 @@ cd "$PROJECT_ROOT"
 git fetch origin main
 git reset --hard origin/main
 
-docker compose down
-docker compose up -d --build --remove-orphans
+sudo docker compose down
+sudo docker compose up -d --build --remove-orphans
 
-docker image prune -f
+sudo docker image prune -f
 echo "Redeployment complete :)"
